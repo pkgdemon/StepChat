3c2f176ae0fa21af1726e030323aa53afeba4ec5
pkgdemon <pkgdemonteam@gmail.com>
2025-07-14 19:31:36 -0500

    Bringup StepChat without interface files

--- main.m
+++ main.m
@@ -1,17 +1,32 @@
-//
-//  main.m
-//  Jabber
-//
-//  Created by David Chisnall on Sun Feb 15 2004.
-//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 #import <AppKit/AppKit.h>
 
 int main(int argc, const char *argv[])
 {
     @autoreleasepool {
-        return NSApplicationMain(argc, argv);
+        NSApplication *app = [NSApplication sharedApplication];
+        
+        // Create a simple window for testing
+        NSWindow *window = [[NSWindow alloc] 
+            initWithContentRect:NSMakeRect(100, 100, 400, 300)
+            styleMask:NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskResizable
+            backing:NSBackingStoreBuffered
+            defer:NO];
+        
+        [window setTitle:@"StepChat - Test"];
+        [window makeKeyAndOrderFront:nil];
+        
+        // Add a simple label
+        NSTextField *label = [[NSTextField alloc] initWithFrame:NSMakeRect(50, 150, 300, 30)];
+        [label setStringValue:@"StepChat is running! Interface files needed."];
+        [label setBezeled:NO];
+        [label setDrawsBackground:NO];
+        [label setEditable:NO];
+        [label setSelectable:NO];
+        
+        [[window contentView] addSubview:label];
+        
+        [app run];
+        return 0;
     }
 }
